cadde8be022da79fde6fa01916273b7fcec3c5ab
Wen <1598933611@qq.com>
2021-11-21 11:47:10 +0800

    feat:bridge

---
+++ src/main/java/com/wen/structural/p2braidge/AdvancedRemote.java
@@ -0,0 +1,13 @@
+package com.wen.structural.p2braidge;
+
+public class AdvancedRemote extends BasicRemote {
+
+    public AdvancedRemote(Device device) {
+        super.device = device;
+    }
+
+    public void mute() {
+        System.out.println("Remote: mute");
+        device.setVolume(0);
+    }
+}
---
+++ src/main/java/com/wen/structural/p2braidge/Remote.java
@@ -0,0 +1,13 @@
+package com.wen.structural.p2braidge;
+
+public interface Remote {
+    void power();
+
+    void volumeDown();
+
+    void volumeUp();
+
+    void channelDown();
+
+    void channelUp();
+}
